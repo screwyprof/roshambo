5f66ae8927d4f182d3029752e8b822224630566d
Max O'Donnell <screwyprof@gmail.com>
2019-03-14 22:36:26 +0300

    DDD/CQRS specific interfaces defined

---
+++ pkg/domain/domain.go
@@ -0,0 +1,66 @@
+package domain
+
+import "fmt"
+
+// Identifier an object identifier.
+type Identifier interface {
+	fmt.Stringer
+}
+
+// Command
+//
+// People request changes to the domain by sending commands.
+// Command are named with a verb in the imperative mood, for example ConfirmOrder.
+type Command interface {
+	CommandType() string
+}
+
+// CommandHandler
+//
+// A command handler receives a command and brokers a result from the appropriate aggregate.
+// "A result" is either a successful application of the command, or an error.
+//
+// Could be implemented like a method on the aggregate.
+type CommandHandler interface {
+	Handle(c Command) ([]DomainEvent, error)
+}
+
+// CommandHandlerFunc is a function that can be used as a command handler.
+type CommandHandlerFunc func(Command) ([]DomainEvent, error)
+
+// DomainEvent represents something that took place in the domain.
+//
+// Events are always named with a past-participle verb, such as OrderConfirmed.
+type DomainEvent interface {
+	EventType() string
+}
+
+// EventApplier applies the given events to an aggregate.
+type EventApplier interface {
+	Apply(e ...DomainEvent) error
+}
+
+// EventApplierFunc is a function that can be used as an event applier.
+type EventApplierFunc func(DomainEvent)
+
+// Aggregate
+//
+// A larger unit of encapsulation than just a class.
+// Every transaction is scoped to a single aggregate.
+// The lifetimes of the components of an aggregate are bounded by
+// the lifetime of the entire aggregate.
+//
+// Concretely, an aggregate will handle commands, apply events,
+// and have a state model encapsulated within it that allows it to implement the required command validation,
+// thus upholding the invariants (business rules) of the aggregate.
+type Aggregate interface {
+	AggregateID() Identifier
+}
+
+// AdvancedAggregate is an aggregate which handles commands
+// and applies events after it automatically
+type AdvancedAggregate interface {
+	Aggregate
+	CommandHandler
+	EventApplier
+}
