eadb072fe1048f4c8a72bce051889f81b450ce03
Max O'Donnell <screwyprof@gmail.com>
2019-03-16 03:08:33 +0300

    EventStore loads events for the given aggregate

--- internal/pkg/cqrs/eventstore/in_memory_event_store.go
+++ internal/pkg/cqrs/eventstore/in_memory_event_store.go
@@ -1,22 +1,38 @@
 package eventstore
 
-import "github.com/screwyprof/roshambo/pkg/domain"
+import (
+	"sync"
+
+	"github.com/screwyprof/roshambo/pkg/domain"
+)
 
 // InMemoryEventStore stores and loads events from memory.
-type InMemoryEventStore struct{}
+type InMemoryEventStore struct {
+	eventStreams   map[domain.Identifier][]domain.DomainEvent
+	eventStreamsMu sync.RWMutex
+}
 
 // NewInInMemoryEventStore creates a new instance of InMemoryEventStore.
 func NewInInMemoryEventStore() *InMemoryEventStore {
-	return &InMemoryEventStore{}
+	return &InMemoryEventStore{
+		eventStreams: make(map[domain.Identifier][]domain.DomainEvent),
+	}
 }
 
 // LoadEventsFor loads events for the given aggregate.
 func (s *InMemoryEventStore) LoadEventsFor(aggregateID domain.Identifier) ([]domain.DomainEvent, error) {
-	return nil, nil
+	s.eventStreamsMu.RLock()
+	defer s.eventStreamsMu.RUnlock()
+
+	return s.eventStreams[aggregateID], nil
 }
 
 //StoreEventsFor saves evens of the given aggregate.
 func (s *InMemoryEventStore) StoreEventsFor(
 	aggregateID domain.Identifier, version int, events []domain.DomainEvent) error {
+	s.eventStreamsMu.Lock()
+	defer s.eventStreamsMu.Unlock()
+
+	s.eventStreams[aggregateID] = events
 	return nil
 }
--- internal/pkg/cqrs/eventstore/in_memory_event_store_test.go
+++ internal/pkg/cqrs/eventstore/in_memory_event_store_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/screwyprof/roshambo/internal/pkg/assert"
+	"github.com/screwyprof/roshambo/internal/pkg/cqrs/aggregate/testdata"
 	"github.com/screwyprof/roshambo/internal/pkg/cqrs/eventstore"
 
 	"github.com/screwyprof/roshambo/pkg/domain"
@@ -16,5 +17,25 @@ func TestNewInInMemoryEventStore(t *testing.T) {
 	t.Run("ItCreatesEventStore", func(t *testing.T) {
 		es := eventstore.NewInInMemoryEventStore()
 		assert.True(t, es != nil)
+	})
+}
+
+func TestInMemoryEventStoreLoadEventsFor(t *testing.T) {
+	t.Run("ItLoadsEventsForTheGivenAggregate", func(t *testing.T) {
+		// assert
+		ID := testdata.StringIdentifier("TestAgg")
+		es := eventstore.NewInInMemoryEventStore()
+
+		want := []domain.DomainEvent{testdata.SomethingHappened{}}
+
+		err := es.StoreEventsFor(ID, 1, want)
+		assert.Ok(t, err)
+
+		// act
+		got, err := es.LoadEventsFor(ID)
+
+		// arrange
+		assert.Ok(t, err)
+		assert.Equals(t, want, got)
 	})
 }
