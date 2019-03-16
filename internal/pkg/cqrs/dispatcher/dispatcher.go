20c94e6c637b0c6075921382bccac7f160f4744f
Max O'Donnell <screwyprof@gmail.com>
2019-03-16 20:14:29 +0300

    Dispatcher requires event store and aggregate factory

--- internal/pkg/cqrs/dispatcher/dispatcher.go
+++ internal/pkg/cqrs/dispatcher/dispatcher.go
@@ -1,1 +1,33 @@
 package dispatcher
+
+import "github.com/screwyprof/roshambo/pkg/domain"
+
+// Dispatcher is a basic message dispatcher.
+//
+// It drives the overall command handling and event application/distribution process.
+// It is suitable for a simple, single node application that can safely build its subscriber list
+// at startup and keep it in memory.
+// Depends on some kind of event storage mechanism.
+type Dispatcher struct {
+	eventStore       domain.EventStore
+	aggregateFactory domain.AggregateFactory
+}
+
+func NewDispatcher(eventStore domain.EventStore, aggregateFactory domain.AggregateFactory) *Dispatcher {
+	if eventStore == nil {
+		panic("eventStore is required")
+	}
+
+	if aggregateFactory == nil {
+		panic("aggregateFactory is required")
+	}
+
+	return &Dispatcher{
+		eventStore:       eventStore,
+		aggregateFactory: aggregateFactory,
+	}
+}
+
+func (d *Dispatcher) Handle(c domain.Command) ([]domain.DomainEvent, error) {
+	return nil, nil
+}
--- internal/pkg/cqrs/dispatcher/dispatcher_test.go
+++ internal/pkg/cqrs/dispatcher/dispatcher_test.go
@@ -1,1 +1,29 @@
-package dispatcher
+package dispatcher_test
+
+import (
+	"testing"
+
+	"github.com/screwyprof/roshambo/internal/pkg/assert"
+	"github.com/screwyprof/roshambo/internal/pkg/cqrs/dispatcher"
+
+	"github.com/screwyprof/roshambo/pkg/domain"
+)
+
+// ensure that Dispatcher  implements domain.CommandHandler interface.
+var _ domain.CommandHandler = (*dispatcher.Dispatcher)(nil)
+
+func TestNewDispatcher(t *testing.T) {
+	t.Run("ItPanicsIfEventStoreIsNotGiven", func(t *testing.T) {
+		factory := func() {
+			dispatcher.NewDispatcher(nil, nil)
+		}
+		assert.Panic(t, factory)
+	})
+
+	t.Run("ItPanicsIfAggregateFactoryIsNotGiven", func(t *testing.T) {
+		factory := func() {
+			dispatcher.NewDispatcher(nil, nil)
+		}
+		assert.Panic(t, factory)
+	})
+}
